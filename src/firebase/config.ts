0002efdf472fc0b80c57d755bc47e83a30aaa359
Devendra Pandey <info.devendrakumarpandey@gmail.com>
2025-11-05 05:23:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8845541332-edbf7",
+  "appId": "1:975995532343:web:f19841b0094d17ff9ffbb2",
+  "apiKey": "AIzaSyDX_CJ4hUmYP9xwvQ2ltevk_8XhklySQaI",
+  "authDomain": "studio-8845541332-edbf7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "975995532343"
+};
